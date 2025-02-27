a94539d249d0dda6161e39fd3581a2c516d490c7
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-02-27 22:37:13 +0600

    first basic AAA test implemented

--- Unit test/AddNum.test.js
+++ Unit test/AddNum.test.js
@@ -1,7 +1,19 @@
 import { it, expect } from "vitest";
 import { addNum } from "./AddNum";
 
-it("should return sum of array", () => {
-  const res = addNum([1, 2, 3]);
-  expect(6).toBe(6);
+// it("should return sum of array", () => {
+//   const res = addNum([1, 2, 3]);
+//   expect(6).toBe(6);
+// });
+
+// AAA pattern
+// Arrange , Action , Assertion
+it("should return sum of array!!!", () => {
+  const numData = [2, 4, 6, 8];
+
+  const expectedRes = numData.reduce((acc, curr) => acc + curr, 0);
+
+  const resSum = addNum(numData);
+
+  expect(resSum).toBe(expectedRes);
 });
